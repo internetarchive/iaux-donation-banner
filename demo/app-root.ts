4c9c508e78960571748483029203246b4f50d368
Jason Buckner <jason@jasonbuckner.com>
2021-10-18 16:33:17 -0700

    Start work on donation banner component

--- demo/app-root.ts
+++ demo/app-root.ts
@@ -1,15 +1,11 @@
 import { html, css, LitElement } from 'lit';
 import { customElement } from 'lit/decorators.js';
-import '../src/your-webcomponent';
+import '../src/donation-banner';
 
 @customElement('app-root')
 export class AppRoot extends LitElement {
   render() {
-    return html`
-      <your-webcomponent title="Hello">
-        <div slot="my-slot">Some slotted content</div>
-      </your-webcomponent>
-    `;
+    return html` <donation-banner> </donation-banner> `;
   }
 
   static styles = css`
